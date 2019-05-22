11d6a5a532a5a1ea3a85e00ac064173431e34325
neropsys <neropsys@gmail.com>
2019-05-23 00:09:31 +0900

    added privacy setting api

--- Capston2/Controllers/UserInfoController.cs
+++ Capston2/Controllers/UserInfoController.cs
@@ -5,15 +5,120 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Capston2DataAccess;
+using Newtonsoft.Json;
+
 namespace Capston2.Controllers
 {
     public class UserInfoController : ApiController
     {
-        public IEnumerable<USER_TABLE> Get()
+
+        public class ResponseFormat
         {
-            using(UserDataEntities entities = new UserDataEntities())
+            public string dateofbirth { get; set; }
+            public string residence { get; set; }
+            public string major { get; set; }
+            public string hobby { get; set; }
+        }
+        public class RequestFormat
+        {
+            public bool dateofbirth { get; set; }
+            public bool residence { get; set; }
+            public bool major { get; set; }
+            public bool hobby { get; set; }
+        }
+        [HttpPost]
+        [Route("api/UserInfo/{userId}")]
+        public HttpResponseMessage SetUserInfo(RequestFormat value, [FromUri]string userId)
+        {
+            using (capston_databaseEntities userDataEntites = new capston_databaseEntities())
             {
-                return entities.USER_TABLE.ToList();
+                var userInfo = userDataEntites.USER_INFO.FirstOrDefault(x => x.id == userId);
+
+                if (userInfo != null)
+                {
+                    using (capston_database_PrivacyEntities privacyEntities = new capston_database_PrivacyEntities())
+                    {
+                        var privacySetting = privacyEntities.USER_INFO_PRIVACY.SingleOrDefault(x => x.id == userId);
+
+                        if (privacySetting != null)
+                        {
+                            privacySetting.dateofbirth = value.dateofbirth ? 1 : (byte?)null;
+                            privacySetting.hobby = value.hobby ? 1 : (byte?)null;
+                            privacySetting.major = value.major ? 1 : (byte?)null;
+                            privacySetting.residence = value.residence ? 1 : (byte?)null;
+                        }
+                        else
+                        {
+
+                            privacyEntities.USER_INFO_PRIVACY.Add(new USER_INFO_PRIVACY
+                            {
+                                dateofbirth = value.dateofbirth ? 1 : (byte?)null,
+                                hobby = value.hobby ? 1 : (byte?)null,
+                                id = userId,
+                                major = value.major ? 1 : (byte?)null,
+                                residence = value.residence ? 1 : (byte?)null
+                            });
+                        }
+                        privacyEntities.SaveChanges();
+                        return new HttpResponseMessage(HttpStatusCode.OK);
+                    }
+                }
+                else
+                {
+                    return new HttpResponseMessage(HttpStatusCode.BadRequest);
+                }
+            }
+        }
+
+        [HttpPost]
+        [Route("api/UserInfo")]
+        public HttpResponseMessage GetUserInfo(HttpRequestMessage request)
+        {
+            var userId = request.Content.ReadAsStringAsync().Result;
+
+            using(capston_databaseEntities userDataEntites = new capston_databaseEntities())
+            {
+                var userInfo = userDataEntites.USER_INFO.FirstOrDefault(x => x.id == userId);
+                
+                if(userInfo != null)
+                {
+                    using (capston_database_PrivacyEntities privacyEntities = new capston_database_PrivacyEntities())
+                    {
+                        HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+                        ResponseFormat retValue = new ResponseFormat();
+                        var privacySetting = privacyEntities.USER_INFO_PRIVACY.FirstOrDefault(x => x.id == userId);
+
+                        if(privacySetting != null)
+                        {
+                            //1:public. 2:friend only. 0 or null:hidden. only 1 or null for now
+                           if(privacySetting.dateofbirth.HasValue)
+                            {
+                                retValue.dateofbirth = userInfo.dateofbirth;
+                            }
+                            if (privacySetting.hobby.HasValue)
+                            {
+                                retValue.hobby = userInfo.hobby;
+                            }
+                            if (privacySetting.major.HasValue)
+                            {
+                                retValue.major = userInfo.major;
+                            }
+                            if (privacySetting.residence.HasValue)
+                            {
+                                retValue.residence = userInfo.residence;
+                            }
+                        }
+
+                        string jsonContent = JsonConvert.SerializeObject(retValue);
+
+                        response.Content = new StringContent(jsonContent, System.Text.Encoding.UTF8, "application/json");
+                        return response;
+                    }
+                }
+                else
+                {
+                    return new HttpResponseMessage(HttpStatusCode.BadRequest);
+                }
             }
         }
     }
